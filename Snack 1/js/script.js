5994809e0b29c0037c4ed57d7e59808126eca339
DaniAm94 <dani.amico94@gmail.com>
2024-01-16 16:42:06 +0100

    completed 1st snack

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -1,1 +1,24 @@
-console.log('JS OK 1!')
+const tableName = 'Tavolo Vip';
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+
+const guestsList = guests.map((Element, index) => {
+    const guest = {
+        table: tableName,
+        guestName: Element,
+        seat: index + 1,
+    };
+    return guest;
+});
+console.log(guestsList);
